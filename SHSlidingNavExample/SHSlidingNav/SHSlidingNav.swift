d572be44fd59511a52baa6f7fb4ce36e3229f928
subhajitregor <subhajit.regor.official@gmail.com>
2017-05-29 21:00:12 +0530

    initial commit

---
+++ SHSlidingNavExample/SHSlidingNav/SHSlidingNav.swift
@@ -0,0 +1,62 @@
+//
+//  SHSlidingNav.swift
+//  SHSlidingNavExample
+//
+//  Created by subhajit halder on 29/05/17.
+//  Copyright © 2017 SubhajitHalder. All rights reserved.
+//
+
+import UIKit
+
+@objc enum SHSlidingNavDirection: Int {
+    case leftToRight = 0
+    case righToLeft
+}
+
+
+class SHSlidingNav: UIViewController {
+    
+    private var isOpen: Bool = false
+    private let viewBackground = UIView()
+    private let leftEdgePanGesture = UIScreenEdgePanGestureRecognizer()
+    private let rightEdgePanGesture = UIScreenEdgePanGestureRecognizer()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.leftEdgePanGesture.addTarget(self, action: #selector(self.handleLeftEdgePan(_:)))
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK: Private Methods
+    @objc private func handleLeftEdgePan(_ leftPan: UIScreenEdgePanGestureRecognizer) {
+        
+    }
+    
+    @objc private func handleRightEdgePan(_ rightPan: UIScreenEdgePanGestureRecognizer) {
+        
+    }
+    
+    
+    
+    //MARK: Public Methods
+    @objc func addTarget(_ viewController: UIViewController, direction: SHSlidingNavDirection) -> SHSlidingNav  {
+        
+        
+        return self
+    }
+    
+    @objc func setFrame(_ left: CGFloat,_ right: CGFloat,_ top: CGFloat,_ bottom: CGFloat) {
+        
+    }
+    
+    @objc func setWidthInRelationToScreen(percent: Float) {
+        
+    }
+
+}
